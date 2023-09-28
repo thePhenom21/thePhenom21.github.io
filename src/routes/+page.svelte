b5c4aff183bf9e6ec4cfa1d90de04cf1cf480b9d
Can Ersoy <can.ersoy@tum.de>
2023-09-28 17:02:38 +0300

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let theme = 'default';
+	let days = '1';
+	let level = '1';
+</script>
+
+<div class="custom_prompt">
+	<label> Theme </label>
+	<input type="text" bind:value={theme} />
+	<label> Days </label>
+	<input type="text" bind:value={days} />
+	<label> Level </label>
+	<input type="text" bind:value={level} />
+</div>
+
+<style>
+	.custom_prompt {
+		display: grid;
+	}
+
+	label,
+	input {
+		grid-column: 1 / 2;
+		margin: 5px;
+	}
+</style>
